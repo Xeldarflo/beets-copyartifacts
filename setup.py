fc31bc538e5aa92d2bc5369055dc25059af3455a
Xeldarflo <34825290+Xeldarflo@users.noreply.github.com>
2026-09-04 02:20:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 
     packages=['beetsplug'],
     namespace_packages=['beetsplug'],
-    install_requires=['beets==2.5.1'],
+    install_requires=['beets>=2.5.1'],
 
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
